e302295957c3f1df97d9a81964b1ecc91a5abdc9
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-11-12 22:11:34 +0300

    Added scraper builder structure

--- scraper/index.ts
+++ scraper/index.ts
@@ -1,8 +1,22 @@
-const axios = require('axios');
+import Scraper from "./scraper"
 
-function main(): void {
-    console.log('hello world');
-    
+async function main(): Promise<void> {
+    const baseURL = 'http://www.arabalar.com.tr/araba-fiyatlari';
+    const selector = '#fiyatlar .marka';
+    const children = ['.name', 'h2', 'a'];
+    const attribute = 'href';
+
+    const hrefs = await Scraper.create(baseURL)
+        .select(selector)
+        .child(...children)
+        .attr(attribute)
+        .scrape();
+        // .nextPhase()
+        // .select()
+        // .child()
+        // .child();
+
+    console.log(hrefs);
 }
 
 main()

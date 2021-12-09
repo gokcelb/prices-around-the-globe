04bb588e469cd02d485a8fa152c46506b2628113
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-12-09 22:48:59 +0300

    Add car component and search component'

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -1,30 +1,37 @@
 <template>
   <div>
-    <input @keydown="onEnter" placeholder="Search for a commodity">
+    <input
+      v-model="searchValue"
+      @keypress="onEnter"
+      placeholder="Search for a commodity"
+    />
+    <button @click="search">Search</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Search.vue",
+  name: "search",
+  props: [ 'searchQuery' ],
   data() {
     return {
-
-    }
+      searchValue: "",
+    };
   },
   methods: {
     onEnter: (event) => {
       if (event.key === "Enter") {
-        this.destructureSearchedValue()
+        this.destructureSearchedValue();
       }
     },
-    destructureSearchedValue: () => {
-
+    search: async () => {
+      console.log("search function ran")
+      const response = await this.searchQuery(this.searchValue)
+      this.$emit('search', response)
     },
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-
 </style>

d42b29ba7b4a656557148b292d6faa91481cf59f
gokcelb <gokcelbilgin@gmail.com>
2021-12-11 20:34:20 +0300

    added AppHeader and RightBar components, styled components

---
+++ client/src/components/RightBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="side-bar">
+    <h3>Would you like to know more about these?</h3>
+    <a>BMW 1 series in Turkey</a>
+    <a>Ford Focus in the US</a>
+    <a>Honda Civic in Japan</a>
+    <a>Minimum wage in Canada</a>
+    <a>Population of Indonesia</a>
+    <a>Purchasing power of Turkey</a>
+    <a>Minimum wage of the US</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "right-bar"
+}
+</script>
+
+<style scoped>
+.side-bar {
+  height: 270px;
+  width: 12%;
+  position: absolute;
+  margin-top: 82px;
+  margin-left: 20px;
+  top: 0;
+  right: 50px;
+  padding: 10px 10px;
+  border-width: 1px;
+  border-color: lightgrey;
+  border-style: solid;
+  display: block;
+}
+
+.side-bar > a {
+  display: block;
+  font-size: 14px;
+  line-height: 2;
+}
+
+.side-bar > a:hover {
+  cursor: pointer;
+}
+</style>
--- client/src/pages/App.vue
+++ client/src/pages/App.vue
@@ -1,50 +1,116 @@
 <template>
   <div id="app">
-    <AppHeader></AppHeader>
-    <search @search="onQuerySearch" :search-query="mockSearchQuery"></search>
-    <ul>
-      <li v-for="(car, index) in queryResponse" :key="index">
-        <p>{{ car.model }}</p>
-        <car-card :car="car" car-title="car.title"></car-card>
-      </li>
-    </ul>
+    <app-header></app-header>
+    <div class="content">
+      <div class="main">
+        <ul>
+          <li v-for="(car, index) in queryResponse" :key="index">
+            <car-card :car="car"></car-card>
+          </li>
+        </ul>
+      </div>
+      <right-bar></right-bar>
+    </div>
   </div>
 </template>
 
 <script>
-import AppHeader from "@/components/AppHeader.vue";
-import Search from "@/components/Search.vue";
-import CarCard from "@/components/CarCard.vue";
+import AppHeader from "../components/AppHeader.vue";
+import CarCard from "../components/CarCard.vue";
+import RightBar from "../components/RightBar.vue";
 
 export default {
   name: "App",
   components: {
     AppHeader,
-    Search,
     CarCard,
+    RightBar,
   },
   data() {
     return {
       queryResponse: [
         {
-          make: "Make",
-          model: "Hyundai",
-          currency: "TRY",
-          price: 121321,
+          make: "Hyundai",
+          model: "i20",
+          currency: "₺",
+          price: 120000,
+          imageURL: require("../../public/somecar.jpeg"),
+        },
+        {
+          make: "BMW",
+          model: "1 serisi",
+          currency: "₺",
+          price: 300000,
+          imageURL: require("../../public/somecar.jpeg"),
+        },
+        {
+          make: "Ford",
+          model: "Focus",
+          currency: "₺",
+          price: 135000,
+          imageURL: require("../../public/somecar.jpeg"),
+        },
+        {
+          make: "BMW",
+          model: "1 serisi",
+          currency: "₺",
+          price: 300000,
+          imageURL: require("../../public/somecar.jpeg"),
+        },
+        {
+          make: "BMW",
+          model: "1 serisi",
+          currency: "₺",
+          price: 300000,
+          imageURL: require("../../public/somecar.jpeg"),
+        },
+        {
+          make: "BMW",
+          model: "1 serisi",
+          currency: "₺",
+          price: 300000,
+          imageURL: require("../../public/somecar.jpeg"),
+        },
+        {
+          make: "BMW",
+          model: "1 serisi",
+          currency: "₺",
+          price: 300000,
+          imageURL: require("../../public/somecar.jpeg"),
         },
       ],
     };
   },
   methods: {
-    onQuerySearch: function (response) {
+    onQueryResult: function (response) {
       console.log(response);
     },
-    mockSearchQuery: async function (query) {
-      console.log(`query= ${query}`);
-    },
+    mockSearchQuery: function (query) {
+      return {query}
+    }
   },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Arial",sans-serif;
+  font-size: 13px;
+}
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.content {
+  width: 85%;
+  margin: auto;
+}
+
+.main {
+  margin-right: 150px;
+}
 </style>

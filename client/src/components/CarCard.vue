be7d51ce44a60ba1ed38ec13cb307192aa6add1e
gokcelb <gokcelbilgin@gmail.com>
2021-12-21 14:25:26 +0300

    changed how car data is displayed in Category component

--- client/src/components/CarCard.vue
+++ client/src/components/CarCard.vue
@@ -1,30 +1,29 @@
 <template>
   <div class="car-card">
-    <img class="car-img" alt="some car" src="../../public/somecar.jpeg">
+<!--    <img class="car-img" alt="some car" src="../../public/somecar.jpeg">-->
     <div class="car-info">
-      <p>{{ formattedCarTitle }}</p>
+      <p>{{ car.make }}</p>
       <p>{{ car.currency }}{{ car.price }}</p>
     </div>
     <div class="car-details">
       <i class="bi arrow" @click="toggleDetails" :class="{'bi-caret-down-fill': isClicked, 'bi-caret-right-fill': !isClicked}">Details</i>
       <div class="details" v-if="detailsToggled">
+        <div v-if="car.modelFeatures">
+          <p><span class="details-title">Model</span>: {{ car.modelFeatures }}</p>
+        </div>
         <div v-if="car.year">
-          <p>Year: {{ car.year }}</p>
+          <p><span class="details-title">Year</span>: {{ car.year }}</p>
         </div>
         <div v-if="car.mileage">
-          <p>Mileage: {{ car.mileage }}</p>
+          <p><span class="details-title">Mileage</span>: {{ car.mileage }}</p>
         </div>
         <div v-if="car.fuel">
-          <p>Fuel Type: {{ car.fuel }}</p>
-        </div>
-        <div v-if="car.features">
-          <p>Engine: {{ car.features }}</p>
+          <p><span class="details-title">Fuel Type</span>: {{ car.fuel }}</p>
         </div>
       </div>
     </div>
   </div>
 </template>
-
 
 <script>
 export default {
@@ -45,9 +44,9 @@ export default {
     }
   },
   computed: {
-    formattedCarTitle: function () {
-      return `${this.car.make} ${this.car.model}`;
-    }
+    // formattedCarInfo: function () {
+    //   return `${this.car.make} ${this.car.model} ${this.car.features}`;
+    // }
   },
 };
 </script>
@@ -55,9 +54,10 @@ export default {
 <style scoped>
 .car-card {
   float: left;
-  min-height: 150px;
+  min-height: 50px;
   height: auto;
-  max-width: 180px;
+  max-width: 150px;
+  width: 150px;
   margin: 2% 1%;
   padding: 10px;
 }
@@ -85,6 +85,11 @@ export default {
   margin-left: 10px;
   margin-top: 10px;
   color: grey;
+}
+
+.details-title {
+  /*text-decoration: underline;*/
+  /*font-style: italic;*/
 }
 
 .details {
--- client/src/views/Category.vue
+++ client/src/views/Category.vue
@@ -2,8 +2,7 @@
   <div id="category">
     <app-header></app-header>
     <div class="content">
-      <h2 style="color: mediumseagreen; margin: 30px 0 10px 30px">Categories</h2>
-      <h3 style="margin: 30px 0 10px 40px;">{{ $route.params.category }}</h3>
+      <h3 class="category-name">{{ capitalizedCategory }}</h3>
       <div class="search-results">
         <ul>
           <li v-for="(carItem, idx) in results" :key="idx">
@@ -34,9 +33,13 @@ export default {
   },
   methods: {
     retrieveCategoryItems: async function () {
-      console.log(this.category)
       this.results = await getCategoryItems(this.category);
     },
+  },
+  computed: {
+    capitalizedCategory: function () {
+      return this.$route.params.category.charAt(0).toUpperCase() + this.$route.params.category.slice(1);
+    }
   },
   mounted () {
     this.retrieveCategoryItems();
@@ -59,5 +62,15 @@ export default {
 
 .search-results > ul > li {
   margin: 10px 20px 10px 0;
+}
+
+.category-name {
+  margin: 30px 60px 10px 20px;
+  background-color: mediumseagreen;
+  color: white;
+  padding: 10px;
+  border-radius: 10px;
+  opacity: 0.9;
+  font-size: 16px;
 }
 </style>
